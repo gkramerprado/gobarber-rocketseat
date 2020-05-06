293fbfebe216de4ab9818fb1e8b24aa06dbfcf71
gkramer14 <cetecdesp@gmail.com>
2020-05-05 22:33:34 -0300

    Criados alguns estilos globais

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div className="App">Pagina Inicial 🌛️</div>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
