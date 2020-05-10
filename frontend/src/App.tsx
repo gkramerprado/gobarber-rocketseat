8e9b535a2349465560eac893d40d2b74f28481ff
gkramer14 <cetecdesp@gmail.com>
2020-05-10 20:20:43 -0300

    Criada tela de cadastro e validacao dos inputs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 const App: React.FC = () => {
   return (
     <>
-      <div className="App">Pagina Inicial 🌛️</div>
+      <SignUp />
       <GlobalStyle />
     </>
   );
---
+++ frontend/src/pages/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <h1>Faca seu logon</h1>
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button>Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default SignIn;
